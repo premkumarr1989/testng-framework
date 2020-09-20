2fc990e916eaa51da5da6b3fc6374bf64a5a79dc
Prem Kumar R <premkumarr1989@gmail.com>
2020-09-20 11:01:44 -0400

    Created Assertions - Assert.assertEquals, Assert.assertTrue, Assert.fail Closes #3

--- src/test/java/org/example/demo/LoginTests.java
+++ src/test/java/org/example/demo/LoginTests.java
@@ -1,5 +1,6 @@
 package org.example.demo;
 
+import junit.framework.AssertionFailedError;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,18 +10,23 @@ public class LoginTests {
         System.out.println("Login test with valid user and password");
         String exp = "Hello";
         String act = "Hello";
-        Assert.assertEquals(exp,act,"The test pass");
+        Assert.assertEquals(exp,act,"The words dont match");
 
     }
 
     @Test(priority = 2,testName = "Login test with invalid user")
     public void loginWithInvalidUser(){
         System.out.println("Login test with invalid user");
+        Assert.fail("Invalid user login test failed");
     }
 
     @Test(priority = 3,testName = "verify home page title")
     public void homepageTest(){
         System.out.println("This is a home page test");
+        String exp = "Homepage";
+        String act = "Homepage";
+        Assert.assertTrue(exp==act, "Home Page title is incorrect");
+
 
     }
 
